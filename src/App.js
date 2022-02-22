eb45d061f6cef34f1db56eccbb42e5491c1ec6a1
Vilmantas Sudaris <vilmantas.sudaris@gmail.com>
2022-02-22 21:57:41 +0200

    upd: set hooks to App component

--- src/App.js
+++ src/App.js
@@ -7,12 +7,23 @@ import Controls from './components/Controls';
 
 
 function App() {
+  const defaultBreakLength = 5;
+  const defaultSessionLength = 25;
+
+  const [ breakTime, setBreakTime ] = useState(defaultBreakLength);
+  const [ sessionTime, setSessionTime ] = useState(defaultSessionLength);
+
+  function handleReset() {
+    setBreakTime(defaultBreakLength);
+    setSessionTime(defaultSessionLength);
+  }
+
   return (
     <div>
       <h1>25 + 5 Clock</h1>
       <div>
-        <LengthSetting name='Break Length' value={breakLength} />
-        <LengthSetting name='Session Length' value={sessionLength} />
+        <LengthSetting name='Break Length' value={breakTime} />
+        <LengthSetting name='Session Length' value={sessionTime} />
       </div>
       <Timer />
       <Controls />
--- src/components/LengthSetting.js
+++ src/components/LengthSetting.js
@@ -1,3 +1,18 @@
+// import { useState } from 'react';
+
 export default function LengthSetting(props) {
-    return null;
+
+
+    return (
+        <div>
+            <div>
+                {props.name}
+            </div>
+            <div>
+                <button>-</button>
+                <span>{props.value}</span>
+                <button>+</button>
+            </div>
+        </div>
+    );
 }
